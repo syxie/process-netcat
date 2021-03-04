7e00dd506e037a98ac51b7f57814e06d5185e50b
Mark Veidemanis <m@zm.is>
2021-03-04 16:22:58 +0000

    Implement server-client protocol

---
+++ README.md
@@ -0,0 +1,83 @@
+Process Netcat
+==============
+
+Flexible program to send process lists between servers.
+```
+Usage: Process Netcat
+
+Options:
+  --version             show program's version number and exit
+  -h, --help            show this help message and exit
+  -c CLIENT, --client=CLIENT
+                        Address to connect to (client mode)
+  -p PORT, --port=PORT  Port to connect to (client mode) or listen on (server
+                        mode)
+  -w WHITELIST, --whitelist=WHITELIST
+                        Whitelist of addresses to allow (server mode)
+  -s, --send            We will send the PS output
+  ```
+
+  When run without arguments, this script will listen on port 1876 and accept connections from anywhere:
+  ```
+» ./main.py 
+[-] Starting up in server mode on port 1876
+```
+
+We can limit this with a whitelist:
+```
+» ./main.py -w 127.0.0.1
+[-] Starting up in server mode on port 1876
+```
+This will only allow connections from `127.0.0.1`.
+
+To connect, let's run this in another pane, or on another machine (changing the IPs):
+```
+» ./main.py -c 127.0.0.1 -s
+[-] Starting up in client mode to 127.0.0.1
+[-] Connection: 127.0.0.1
+[-] Received OK from server
+[-] Starting PS transmission
+[-] Sent tasks
+```
+
+As you can see, you will need to specify the `-s` flag if you would like to send the task output.
+
+If you omit it, both the server and the client will alert you to either both sides, or neither side sending the tasks output:
+
+Server side messages:
+```
+[-] Neither side is sending PS output, please configure the -s flag on one side
+--
+[-] We're both sending PS output, please configure the -s flag only on one side
+```
+
+Client side messages:
+```
+[-] Error from the other side: Neither side is sending PS output, please configure the -s flag on one side
+--
+[-] Error from the other side: We're both sending PS output, please configure the -s flag only on one side
+```
+
+When run successfully, here is the output on the server side:
+```
+» ./main.py   
+[-] Starting up in server mode on port 1876
+[-] Connection: 127.0.0.1
+[-] OK: They are sending PS
+[-] Received tasks from 127.0.0.1
+[-] Received tasks from 127.0.0.1
+[-] Received tasks from 127.0.0.1
+```
+
+And the client side:
+```
+» ./main.py -c 127.0.0.1 -s
+[-] Starting up in client mode to 127.0.0.1
+[-] Connection: 127.0.0.1
+[-] Received OK from server
+[-] Starting PS transmission
+[-] Sent tasks
+[-] Sent tasks
+[-] Sent tasks
+```
+
---
+++ main.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+from twisted.internet import protocol, reactor
+from json import loads, dumps, dump
+from twisted.internet.endpoints import TCP4ClientEndpoint, TCP4ServerEndpoint, connectProtocol
+from twisted.internet.task import LoopingCall
+from subprocess import Popen, PIPE
+import optparse
+import psutil
+
+serverInst = None
+taskSender = None
+
+def getopts():
+    parser = optparse.OptionParser("Process Netcat", version="% 0.1")
+    parser.add_option("-c", "--client", dest="client", type="string", help="Address to connect to (client mode)")
+    parser.add_option("-p", "--port", dest="port", type=int, default=1876, help="Port to connect to (client mode) or listen on (server mode)")
+    parser.add_option("-w", "--whitelist", dest="whitelist", action="append", help="Whitelist of addresses to allow (server mode)")
+    parser.add_option("-s", "--send", dest="send", action="store_true", default=False, help="We will send the PS output")
+    parser.add_option("-f", "--file", dest="file", type="string", default="tasks.json", help="File to store JSON from tasks into")
+    (options, args) = parser.parse_args()
+
+    if options.client:
+        if options.whitelist:
+            log("WARNING - Using client mode but whitelist specified, ignoring")
+        log(f"Starting up in client mode to {options.client}")
+    else:
+        log(f"Starting up in server mode on port {options.port}")
+    return options
+
+def log(data):
+    print(f"[-] {data}")
+
+
+class ProcessNetcat(protocol.Protocol): # This is both a client and a server
+    def connectionMade(self):
+        self.addr = self.transport.getPeer()
+        log(f"Connection: {self.addr.host}")
+        if options.whitelist:
+            if not self.addr.host in options.whitelist:
+                log(f"Peer not whitelisted, dropping connection: {self.addr.host}")
+                self.transport.loseConnection()
+
+    def connectionLost(self, reason):
+        global taskSender
+        log(f"Connection lost: {self.addr.host}: {reason.getErrorMessage()}")
+        if not taskSender == None:
+            taskSender.stop()
+            log(f"Ceasing task sending")
+            taskSender = None
+
+    def connectionFailed(self, reason):
+        log(f"Connection failed: {self.addr.host}: {reason.getErrorMessage()}")
+
+    def parse(self, data):
+        if "type" in data.keys():
+            dtype = data["type"]
+        else:
+            return
+
+        if dtype == "hello":
+            if not "send" in data.keys():
+                return
+            if data["send"] and options.send:
+                msg = f"We're both sending PS output, please configure the -s flag only on one side"
+                log(msg)
+                self.send_err(msg)
+            elif not data["send"] and not options.send:
+                msg = f"Neither side is sending PS output, please configure the -s flag on one side"
+                log(msg)
+                self.send_err(msg)
+            elif options.send:
+                log(f"OK: We are sending PS")
+                self.send_ok()
+                self.send_tasks()
+
+            elif data["send"]:
+                log(f"OK: They are sending PS")
+                self.send_ok()
+
+        elif dtype == "tasks":
+            if "tasks" in data.keys():
+                log(f"Received tasks from {self.addr.host}")
+                self.store_tasks(data["tasks"])
+
+        elif dtype == "err":
+            if not "msg" in data.keys():
+                return
+            log(f"Error from the other side: {data['msg']}")
+
+        elif dtype == "ok":
+            log("Received OK from server")
+            if options.send:
+                log("Starting PS transmission")
+                self.send_tasks()
+
+    def dataReceived(self, data):
+        #log(f"Data received from {self.addr}: {data}")
+        dataSplit = data.split(b"\r\n") # Allow multiple messages at once without getting confused
+        for i in dataSplit:
+            if not i == b"":
+                toParse = loads(i)
+                self.parse(toParse)
+
+    def say(self, data):
+        data += "\r\n"
+        data = data.encode("utf-8", "replace")
+        self.transport.write(data)
+
+    def send_hello(self):
+        hello = dumps({
+            "type": "hello",
+            "send": options.send})
+        self.say(hello)
+        return
+
+    def send_ok(self):
+        msg = dumps({
+            "type": "ok"})
+        self.say(msg)
+        return
+
+    def send_err(self, msg):
+        msg = dumps({
+            "type": "err",
+            "msg": msg})
+        self.say(msg)
+        return
+
+    def send_tasks(self):
+        global taskSender
+        lc = LoopingCall(self._send_tasks)
+        lc.start(5)
+        taskSender = lc
+
+    def _send_tasks(self):
+        tasks = {}
+        for i in psutil.process_iter():
+            tasks[i.pid] = {
+                    "name": i.name(),
+                    "status": i.status(),
+                    "created": i.create_time()}
+        self.say(dumps({
+            "type": "tasks",
+            "tasks": tasks}))
+        log(f"Sent tasks")
+
+    def store_tasks(self, tasks):
+        with open(options.file, "w") as f:
+            dump(tasks, f, indent=4)
+
+class ProcessNetcatFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return ProcessNetcat()
+
+    #def clientConnectionLost(self, addr)
+
+def gotProtocol(p):
+    p.send_hello()
+
+def startListener():
+    global serverInst
+    if options.client:
+        point = TCP4ClientEndpoint(reactor, options.client, options.port)
+        d = connectProtocol(point, ProcessNetcat())
+        d.addCallback(gotProtocol)
+    else:
+        serverInst = ProcessNetcatFactory()
+        endpoint = TCP4ServerEndpoint(reactor, options.port)
+        endpoint.listen(serverInst)
+    #reactor.listenTCP(1237, SpawnFactory())
+    reactor.run()
+
+if __name__ == "__main__":
+    options = getopts()
+    startListener()
